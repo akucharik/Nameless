62e697f12913890c06ee8b94f4cf3e935218b0e0
Adam Kucharik <Adam@Adams-MacBook-Air.local>
2015-02-20 10:04:24 -0500

    Update libraries

--- javascript/config.js
+++ javascript/config.js
@@ -1,10 +1,10 @@
 requirejs.config({
     baseUrl: 'javascript/game',
     paths: {
-        backbone: '../libraries/backbone.min', // v1.1.2
+        backbone: '../libraries/backbone-min', // v1.1.2
         jquery: '../libraries/jquery.min', // v2.1.1
         phaser: '../libraries/phaser.min', // v2.2.2
-        underscore: '../libraries/underscore.min' // v1.6.0
+        underscore: '../libraries/underscore-min' // v1.8.1
     },
     shim: {
         'phaser': {
